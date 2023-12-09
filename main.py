460d671b671e0e7014f9a59be4f15a1d7bb5470c
phillip <phillip.leejy@gmail.com>
2023-12-09 17:47:47 +0900

    complete test.

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import requests
 import boto3
 import os
 import sys
-import io
-import base64
-from PIL import Image
-from datetime import datetime
 
 AWS_ACCESS_KEY_ID = sys.argv[1]
 AWS_SECRET_ACCESS_KEY = sys.argv[2]
@@ -30,16 +26,17 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/53
 payload = {
     "prompt": "jcsla style, light solid background color, realistic, solo photo, full or half body shot, looking at the camera, sharp focus, highly detailed <lora:ssaemi:1>",
     "negative_prompt": "(worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch, various background colors, extra legs, missing legs, accessories, clothes), open mouth, divide photo, grid photo",
+    "steps": 20,
     "cfg_scale": 3,
     "width": 512,
     "height": 512,
+    "restore_faces": False,
     "denoising_strength": 1.5,
     "hr_scale": 1,
+    "send_images": False,
     "save_images": True
 }
 
-output_directory = 'outputs'
-os.makedirs(output_directory, exist_ok=True)
 for i in range(1):
     response = requests.post(url=f'{url}/sdapi/v1/txt2img', headers=headers, json=payload)
 
