16f6c0af0d1413266434d199bd8e699e09bf8510
phillip <phillip.leejy@gmail.com>
2023-12-20 19:21:49 +0900

    update command.

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ ALBUM_ID = sys.argv[4]
 url = "http://localhost:7861"
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'}
 payload = {
-    "prompt": "jcsla style, light solid background color, realistic, solo photo, full or half body shot, looking at the camera, sharp focus, highly detailed <lora:ssaemi:1>",
-    "negative_prompt": "(worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch, various background colors, extra legs, missing legs, accessories, clothes), open mouth, divide photo, grid photo",
+    "prompt": "(jcsla style:1.5), (ONLY ONE light solid background color:1.5, Floor color is same as background one, clear background, not blurry dog), realistic, solo photo, full or half body shot, looking at the camera, highly detailed, dslr photo, 8K <lora:ssaemi:1>",
+    "negative_prompt": "((stuff:1.5, many background color:1.5, blur:1.5, blurry:1.5), sofa, chair, stuff, table, worst quality, low quality, illustration, 3d, 2d, painting, cartoons, sketch, various background colors, extra legs, missing legs, accessories, clothes), open mouth, divide photo, grid photo",
+    "sampler_name": "Euler a",
     "steps": 20,
     "cfg_scale": 3,
-    "width": 512,
-    "height": 512,
-    "restore_faces": False,
+    "width": 1024,
+    "height": 1024,
     "denoising_strength": 1.5,
-    "hr_scale": 1,
+    "hr_scale": 1.5,
+    "hr_upscaler": "R-ESRGAN 4x+",
     "send_images": False,
     "save_images": True
 }
